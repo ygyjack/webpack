b32450167d83a1d2e0264e0ce94234db99e89366
JackYu <ygyjack@gmail.com>
2019-10-27 21:09:12 +0000

    [wp-03]: chapter3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
+/*
+* CHAPTER 1
+*/
 import _ from 'lodash';
+
+/*
+* CHAPTER 2
+*/
 import './style.css';
 import Icon from './icon.svg';
 import Data from './data.xml';
+
+/*
+* CHAPTER 3
+*/
+import printMe from './print.js';
+
 
 function component() {
   let element = document.createElement('div');
@@ -10,11 +23,21 @@ function component() {
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
     
+    
   var myIcon = new Image();
   myIcon.src = Icon;
   element.appendChild(myIcon);
   
   console.log("Data: "+JSON.stringify(Data));
+    
+    
+    var btn = document.createElement('button');
+    btn.innerHTML = '点击这里，然后查看 console！';
+   btn.onclick = printMe;
+
+   element.appendChild(btn);
+
+    
 
   return element;
 }
