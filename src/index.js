e1b9bcd450c91c466daffcd2006feea7fe89a770
JackYu <ygyjack@gmail.com>
2019-10-27 20:32:07 +0000

    [wp-02]: chapter2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.svg';
+import Data from './data.xml';
 
 function component() {
   let element = document.createElement('div');
 
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+    
+  var myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+  
+  console.log("Data: "+JSON.stringify(Data));
 
   return element;
 }
