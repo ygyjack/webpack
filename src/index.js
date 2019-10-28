0332625be07da30b5ebee6b195f3fbd321d49ae8
JackYu <ygyjack@gmail.com>
2019-10-28 14:50:08 +0000

    [wp-06]: chapter6 - tree shaking

--- src/index.js
+++ src/index.js
@@ -1,61 +1,79 @@
-/*
-* CHAPTER 1
-*/
-import _ from 'lodash';
+///*
+//* CHAPTER 1
+//*/
+//import _ from 'lodash';
+///*
+//* CHAPTER 2
+//*/
+//import './style.css';
+//import Icon from './icon.svg';
+//import Data from './data.xml';
+//
+///*
+//* CHAPTER 3
+//*/
+//import printMe from './print.js';
+//
+///*
+//* CHAPTER 6
+//*/
+//import { cube } from './math.js';
+//
+//
+//function component() {
+//  let element = document.createElement('div');
+//
+//  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
+//  var output = _.join(['Hello', 'webpack'], ' ');
+//  element.innerHTML = _.join([output+':', '5 cubed is equal to ' + cube(5)], ' ');
+//  element.classList.add('hello');
+//    
+//  var myIcon = new Image();
+//  myIcon.src = Icon;
+//  element.appendChild(myIcon);
+//  
+//  console.log("Data: "+JSON.stringify(Data));
+//    
+//    
+//    var btn = document.createElement('button');
+//    btn.innerHTML = '点击这里，然后查看 console！';
+//   btn.onclick = printMe;  // onclick 事件绑定原始的 printMe 函数上
+//
+//   element.appendChild(btn);
+//
+//
+//  return element;
+//}
+//
+// let element = component(); // 存储 element，以在 print.js 修改时重新渲染
+// document.body.appendChild(element);
+//
+//
+// if (module.hot) {
+//   module.hot.accept('./print.js', function() {
+//     console.log('Accepting the updated printMe module!');
+//     document.body.removeChild(element);
+//     element = component(); // Re-render the "component" to update the click handler
+//     element = component(); // 重新渲染 "component"，以便更新 click 事件处理函数
+//     document.body.appendChild(element);
+//   })
+// }
+//
+//
 
-/*
-* CHAPTER 2
-*/
-import './style.css';
-import Icon from './icon.svg';
-import Data from './data.xml';
-
-/*
-* CHAPTER 3
-*/
-import printMe from './print.js';
-
-/*
-* CHAPTER 6
-*/
-import { cube } from './math.js';
 
 
-function component() {
-  let element = document.createElement('div');
+ import { cube } from './math.js';
 
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  var output = _.join(['Hello', 'webpack'], ' ');
-  element.innerHTML = _.join([output+':', '5 cubed is equal to ' + cube(5)], ' ');
-  element.classList.add('hello');
-    
-  var myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-  
-  console.log("Data: "+JSON.stringify(Data));
-    
-    
-    var btn = document.createElement('button');
-    btn.innerHTML = '点击这里，然后查看 console！';
-   btn.onclick = printMe;  // onclick 事件绑定原始的 printMe 函数上
+  function component() {
+   var element = document.createElement('pre');
 
-   element.appendChild(btn);
+   element.innerHTML = [
+     'Hello webpack!',
+     '5 cubed is equal to ' + cube(5)
+   ].join('\n\n');
 
+    return element;
+  }
 
-  return element;
-}
-
- let element = component(); // 存储 element，以在 print.js 修改时重新渲染
- document.body.appendChild(element);
-
-
- if (module.hot) {
-   module.hot.accept('./print.js', function() {
-     console.log('Accepting the updated printMe module!');
-     document.body.removeChild(element);
-     element = component(); // Re-render the "component" to update the click handler
-     element = component(); // 重新渲染 "component"，以便更新 click 事件处理函数
-     document.body.appendChild(element);
-   })
- }
+  document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,76 @@
+//const path = require('path');
+//const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+//const webpack = require('webpack');
+//
+//module.exports = {
+//   entry: {
+//     app: './src/index.js'
+//   },
+//   devtool: 'inline-source-map',
+//   devServer: {
+//     contentBase: './dist',
+//     hot: true
+//   },
+//   plugins: [
+//     new CleanWebpackPlugin(),
+//     new HtmlWebpackPlugin({
+//       title: '模块热替换'
+//     }),
+//     new webpack.HotModuleReplacementPlugin()
+//   ],
+//  output: {
+//    filename: '[name].bundle.js',
+//    path: path.resolve(__dirname, 'dist'),
+//    publicPath: '/'
+//  },
+//  module: {
+//    rules: [{
+//      test: /\.css$/,
+//      use: [
+//        'style-loader',
+//        'css-loader'
+//      ]
+//    },
+//    {
+//     test: /\.(png|svg|jpg|gif)$/,
+//     use: [
+//       'file-loader'
+//     ]
+//    },
+//   {
+//     test: /\.(woff|woff2|eot|ttf|otf)$/,
+//     use: [
+//       'file-loader'
+//     ]
+//   },
+//       {
+//         test: /\.(csv|tsv)$/,
+//         use: [
+//           'csv-loader'
+//         ]
+//       },
+//       {
+//         test: /\.xml$/,
+//         use: [
+//           'xml-loader'
+//         ]
+//       }
+//
+//    ]
+//  }
+//
+//};
+//
+//
+
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
- mode: 'development',
- optimization: {
-   usedExports: true
- },
-
-   entry: {
-     app: './src/index.js'
-   },
-   devtool: 'inline-source-map',
-   devServer: {
-     contentBase: './dist',
-     hot: true
-   },
-   plugins: [
-     new CleanWebpackPlugin(),
-     new HtmlWebpackPlugin({
-       title: '模块热替换'
-     }),
-     new webpack.HotModuleReplacementPlugin()
-   ],
+  entry: './src/index.js',
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
-  },
-  module: {
-    rules: [{
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        'css-loader'
-      ]
-    },
-    {
-     test: /\.(png|svg|jpg|gif)$/,
-     use: [
-       'file-loader'
-     ]
-    },
-   {
-     test: /\.(woff|woff2|eot|ttf|otf)$/,
-     use: [
-       'file-loader'
-     ]
-   },
-       {
-         test: /\.(csv|tsv)$/,
-         use: [
-           'csv-loader'
-         ]
-       },
-       {
-         test: /\.xml$/,
-         use: [
-           'xml-loader'
-         ]
-       }
-
-    ]
-  }
-
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+ },
+ mode: 'production'
 };
